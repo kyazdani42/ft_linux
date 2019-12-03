e1913ffabe590ed1bfd67e751838ed96af96c065
kyazdani42 <yazdani.kiyan@protonmail.com>
2019-12-03 14:19:24 +0100

    add program list

--- backup_commands.sh
+++ backup_commands.sh
@@ -16,3 +16,7 @@ mount -v -t ext4 /dev/sdb2 $LFS
 swapon -v /dev/sdb3
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+pushd $LFS/sources
+md5sum -c md5sums
+popd
